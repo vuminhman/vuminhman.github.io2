4c92826e9f0f7c12c9597fbbf5d70766c4e9062d
manvu <man.vu@mohawkcollege.ca>
2023-08-09 17:15:16 -0400

    Add navigation bar & show one component at a time.

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {
+    handleScroll();
+};
+
+function handleScroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("sticky-navbar").style.padding = "10px 10px";
+    } else {
+        document.getElementById("sticky-navbar").style.padding = "20px 10px";
+    }
+}
+
+$(document).ready(function() {
+    $("#sticky-navbar a.nav-link").click(function(event) {
+        // Prevent the default action (i.e., jumping to the section)
+        event.preventDefault();
+        
+        // Hide all sections
+        $("section").hide();
+
+        // Show the targeted section
+        $($(this).attr("href")).show();
+    });
+});
